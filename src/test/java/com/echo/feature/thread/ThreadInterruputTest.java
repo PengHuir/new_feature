6b12d50aa7857898aeeff0e96ce572b5592e5876
zhangpenghui <echo@echodeMacBook-Pro.local>
2023-11-27 17:55:27 +0800

    first commit

---
+++ src/test/java/com/echo/feature/thread/ThreadInterruputTest.java
@@ -0,0 +1,29 @@
+package com.echo.feature.thread;
+
+/**
+ * TODO
+ *
+ * @author ph.zhang
+ * Created by on 2023/11/27 16:29
+ */
+public class ThreadInterruputTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        MyThread myThread = new MyThread();
+        myThread.start();
+        Thread.sleep(1);
+        myThread.interrupt();
+        Thread.sleep(1);
+        System.exit(0);
+    }
+
+    public static class MyThread extends Thread{
+        @Override
+        public void run() {
+           while (true) {
+               boolean interrupted = this.isInterrupted();
+               System.out.println("interrupted = " + interrupted);
+           }
+        }
+    }
+}
